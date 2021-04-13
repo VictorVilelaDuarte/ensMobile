20b46807d81c3786b60fcbdec056d65e6501cf64
Victor Vilela Duarte <victorvduarte@gmail.com>
2021-04-13 00:27:29 -0300

    Pagina com listagem de equipes

--- src/pages/Teams/index.tsx
+++ src/pages/Teams/index.tsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 
-import { Container } from './styles';
+import {
+  Container,
+  List,
+  TeamsContainer,
+  TeamsImageDiv,
+  TeamsImage,
+  TeamsTitle,
+  SeparatorItem,
+} from './styles';
+
+export interface TeamsType {
+  Equipe_ID: number;
+  Equipe_Nome: string;
+  Equipe_imagem: string;
+}
 
 const Teams: React.FC = () => {
-  return <Container />;
+  const [teams, setTeams] = useState<TeamsType[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`/equipe`)
+      .then(res => {
+        res.data.data.map((item: TeamsType) => {
+          const newObj = {
+            Equipe_ID: item.Equipe_ID,
+            Equipe_Nome: item.Equipe_Nome,
+            Equipe_imagem: item.Equipe_imagem,
+          };
+
+          if (newObj.Equipe_ID !== 0) {
+            return setTeams(oldTeams => [...oldTeams, newObj]);
+          }
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <List
+        ItemSeparatorComponent={() => <SeparatorItem />}
+        data={teams}
+        keyExtractor={item => item.Equipe_ID.toString()}
+        renderItem={({ item }) => (
+          <TeamsContainer onPress={() => console.log(typeof item.Equipe_ID)}>
+            <TeamsImageDiv>
+              <TeamsImage source={{ uri: item.Equipe_imagem }} />
+            </TeamsImageDiv>
+            <TeamsTitle>
+              {item.Equipe_ID} - {item.Equipe_Nome}
+            </TeamsTitle>
+          </TeamsContainer>
+        )}
+      />
+    </Container>
+  );
 };
 
 export default Teams;
--- src/pages/Teams/styles.ts
+++ src/pages/Teams/styles.ts
@@ -1,3 +1,43 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
+import { TeamsType } from './index';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const List = styled(FlatList as new () => FlatList<TeamsType>).attrs({
+  contentContainerStyle: {
+    paddingTop: 10,
+    paddingBottom: 16,
+    paddingHorizontal: 24,
+  },
+})``;
+
+export const TeamsContainer = styled.TouchableOpacity`
+  margin-bottom: 16px;
+  margin-top: 16px;
+`;
+
+export const TeamsImageDiv = styled.View`
+  width: 100%;
+  height: 190px;
+`;
+
+export const TeamsImage = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
+
+export const TeamsTitle = styled.Text`
+  color: #326b97;
+  align-self: center;
+  margin-top: 5px;
+  font-size: 18px;
+`;
+
+export const SeparatorItem = styled.View`
+  width: 100%;
+  height: 1px;
+  border: 1px solid #e1e1e1;
+`;
